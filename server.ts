6d8412a7936292c3d2bbf092f3a4def212086814
salasCalacuta <fernandez.leonardoc@gmail.com>
2026-03-14 21:45:50 -0300

    version 1.03
    puesta online ok

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
